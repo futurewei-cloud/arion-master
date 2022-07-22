3879f7a173a446177689557458e33b1a4f8278fd
Dahai Liu <dliu@futurewei.com>
2022-07-22 14:30:54 -0700

    Add vni GoalState

--- arion_master/src/main/java/com/futurewei/arionmaster/grpc/ArionServerService.java
+++ arion_master/src/main/java/com/futurewei/arionmaster/grpc/ArionServerService.java
@@ -6,13 +6,16 @@ import com.futurewei.arion.schema.Arionmaster;
 import com.futurewei.arionmaster.service.GoalStatePersistenceService;
 import com.futurewei.common.executor.AsyncExecutor;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
+@GrpcService
 public class ArionServerService extends ArionMasterServiceGrpc.ArionMasterServiceImplBase {
     private static final Logger logger = LoggerFactory.getLogger(ArionServerService.class);
 
