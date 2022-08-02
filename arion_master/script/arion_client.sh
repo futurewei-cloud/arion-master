3d4347ce29922df156a3debc794ff7de502f3e41
Longzhang Fu <lfu@futurewei.com>
2022-08-02 16:18:32 -0700

    Update license statement

--- arion_master/script/arion_client.sh
+++ arion_master/script/arion_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # MIT License
-# Copyright(c) 2020 Futurewei Cloud
+# Copyright(c) 2022 Futurewei Cloud
 #
 #     Permission is hereby granted,
 #     free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction,
