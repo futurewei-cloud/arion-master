a029d680590d05a4e25a8f982e7a7b550a80a5cd
Dahai Liu <dliu@futurewei.com>
2022-10-04 15:49:12 -0700

    Add ArionMaster banchmark

---
+++ banchmark/src/test/java/com/futurewei/banchmark/BanchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurewei.banchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanchmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
